b842fcb479c569e3fb910017bf2e1489c2af4a91
Aya elagbani <ayaelabnai@gmail.com>
2025-12-31 12:54:20 +0100

    Fix Flask app initialization and update route response

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
-app = Flask(app)
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-  print("hello rahma aya cmc fatimato")
+    return "Hello Rahma 👋 Your Azure App Service is working!"
+
+if __name__ == "__main__":
+    app.run()
